307efa88f26637beb83422fe0054e5234d27abff
Tim Herzig <tim.herzig@hotmail.com>
2025-02-04 15:50:50 +0100

    script to save models statistics
    Signed-off-by: Tim Herzig <tim.herzig@hotmail.com>

--- src/util/misc/model_stats.py
+++ src/util/misc/model_stats.py
@@ -1,3 +1,9 @@
+import os
+import torch
+
+from src.util.utils import get_paths, get_model_and_checkpoint
+
+
 def log_model_stats(config, args):
     """
     Logs stats about the model, such as number of parameters and memory usage.
@@ -9,4 +15,21 @@ def log_model_stats(config, args):
     Returns:
         None
     """
+
+    log_file = os.path.join(args.eval_root, "notes.txt")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    _, _, model_dir = get_paths(
+        config, create_folders=False, evaluate=True, root=args.eval_root
+    )
+    model, _ = get_model_and_checkpoint(config, model_dir, True)
+
+    num_params = sum(p.numel() for p in model.parameters())
+    num_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+    with open(log_file, "a") as f:
+        f.write(f"\nNumber of parameters: {num_params}\n")
+        f.write(f"Number of trainable parameters: {num_trainable_params}\n")
+
     return None
