73559388199b60de36aee0d45a17233b0bc57b91
Tim Herzig <tim.herzig@hotmail.com>
2025-02-18 16:16:45 +0100

    second version of lf multimodal model
    Signed-off-by: Tim Herzig <tim.herzig@hotmail.com>

--- src/model/multimodal_model.py
+++ src/model/multimodal_model.py
@@ -5,12 +5,19 @@ from src.model.model import Model
 
 class Multimodal_Model(nn.Module):
     def __init__(
-        self, audio_config, video_config, audio_weights=None, video_weights=None
+        self,
+        audio_config,
+        video_config,
+        audio_weights=None,
+        video_weights=None,
+        conv_fusion=False,
     ):
         super(Multimodal_Model, self).__init__()
 
         self.audio_config = audio_config
         self.video_config = video_config
+
+        self.conv_fusion = conv_fusion
 
         self.audio_model = Model(audio_config)
         self.video_model = Model(video_config)
@@ -20,9 +27,18 @@ class Multimodal_Model(nn.Module):
         if video_weights is not None:
             self.video_model.load_state_dict(video_weights)
 
-        self.conv = nn.Conv1d(2, 1, 1)
-        self.lin1 = nn.Linear(160, 64)
-        self.lin2 = nn.Linear(64, 4)
+        if self.conv_fusion:
+            self.conv = nn.Conv1d(2, 1, 1)
+            self.lin1 = nn.Linear(160, 64)
+            self.lin2 = nn.Linear(64, 4)
+        else:
+            self.a_fc = nn.Linear(2, 2)
+            self.a_lin = nn.Linear(2, 1)
+            self.a_out_layer = nn.Linear(160, 2)
+
+            self.v_fc = nn.Linear(2, 2)
+            self.v_lin = nn.Linear(2, 1)
+            self.v_out_layer = nn.Linear(160, 2)
 
     def forward(self, x):
         a, v = x
@@ -33,9 +49,24 @@ class Multimodal_Model(nn.Module):
 
         av = torch.cat((a, v), dim=1)
 
-        x = self.conv(av).squeeze(1)
-        x = torch.relu(self.lin1(x))
-        x = self.lin2(x)
-        x = x.reshape(-1, 2, 2)
+        if self.conv_fusion:
+            x = self.conv(av).squeeze(1)
+            x = torch.relu(self.lin1(x))
+            x = self.lin2(x)
+            x = x.reshape(-1, 2, 2)
 
-        return x
+            return x
+
+        else:
+            av = av.transpose(1, 2)
+
+            a = self.a_fc(av)
+            a = torch.relu(self.a_lin(a)).squeeze(2)
+            a = self.a_out_layer(a)
+
+            v = self.v_fc(av)
+            v = torch.relu(self.v_lin(v)).squeeze(2)
+            v = self.v_out_layer(v)
+
+            av = torch.stack([a, v], dim=1)
+            return av
